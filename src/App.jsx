32176cd3931c1215a4a7f7440c067c2f6250672b
JIWI-CHOUX <als.dn960418@gmail.com>
2024-08-16 14:12:28 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './component/Header';
+import Content from './component/Content';
+import Footer from './component/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Content></Content>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
